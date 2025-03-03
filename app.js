3efa43da5136257a03cf330cbeb286ed93c0cd51
Nicolas DURAND <nicolas.durand.93.01@gmail.com>
2025-03-03 10:45:56 +0100

    finalisation back-end

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+require('./models/connection.js')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
