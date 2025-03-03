36528b0c61e34fde587404c9772725b50da3b8ac
Chaudry Mohammadgit config --global user.email chaudrymohammad.s@gmail.compwdgit config --global user.name Chaudry <chaudrymohammad.s@gmail.com>
2025-03-03 14:03:36 +0100

    modif scheme roles

--- models/users.js
+++ models/users.js
@@ -1,25 +1,29 @@
-const mongoose = require('mongoose');
-const uid2 = require('uid2');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    token: { type: String, default: () => uid2(32) },
-    roleId: { type: mongoose.Schema.Types.ObjectId, ref: "roles", required: true },
-    serviceId: { type: mongoose.Schema.Types.ObjectId, ref: "services" },
-    ticketsAssigned: [{ type: mongoose.Schema.Types.ObjectId, ref: "tickets" }],
-    email: { type: String, required: true, unique: true },
-    isActive: { type: Boolean, default: true },
-    createdDate: { type: Date, default: Date.now },
-    createdBy: { type: String, required: true },
-  });
-
-userSchema.pre('save', async function (next) { 
-  this.password = bcrypt.hash(this.password); 
-next(); 
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  token: { type: String, default: () => uid2(32) },
+  roleId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "roles",
+    required: true,
+  },
+  serviceId: { type: mongoose.Schema.Types.ObjectId, ref: "services" },
+  ticketsAssigned: [{ type: mongoose.Schema.Types.ObjectId, ref: "tickets" }],
+  email: { type: String, required: true, unique: true },
+  isActive: { type: Boolean, default: true },
+  createdDate: { type: Date, default: Date.now },
+  createdBy: { type: String, required: true },
 });
 
-const User = mongoose.model('users', userSchema);
+userSchema.pre("save", async function (next) {
+  this.password = bcrypt.hash(this.password);
+  next();
+});
+
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
